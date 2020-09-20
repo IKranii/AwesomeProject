1a6b257ebb865ef5fa75364b03de7c5fc9fd6605
IKranii <51850489+IKranii@users.noreply.github.com>
2020-09-20 08:56:30 +0700

    19sep2020

--- screens/Ex06Screen.js
+++ screens/Ex06Screen.js
@@ -6,13 +6,13 @@ export default function Ex06Screen({navigation}) {
     
 
     return (
-        <View style={{ flex: 1, flexDirection: 'column', justifyContent:'center', alignItems:'center' }}>
-      <View
-        style={{ backgroundColor: '#50E3C2', width: 100, height: 100 }}></View>
-      <View
-        style={{ backgroundColor: '#4A90E2', width: 100, height: 100 }}></View>
-      <View
-        style={{ backgroundColor: '#9013FE', width: 100, height: 100 }}></View>
+    <View style={{ flex: 1, flexDirection: 'column', justifyContent:'center', alignItems:'center' }}>
+          <View
+            style={{ backgroundColor: '#50E3C2', width: 100, height: 100 }}></View>
+          <View
+            style={{ backgroundColor: '#4A90E2', width: 100, height: 100 }}></View>
+          <View
+            style={{ backgroundColor: '#9013FE', width: 100, height: 100 }}></View>
     </View>
        
     );
